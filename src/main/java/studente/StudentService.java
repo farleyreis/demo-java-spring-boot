2f09ad1aa849c7157ffa0424967df00814c7811b
Farley Reis <farley_reis@Farleys-MacBook-Pro.local>
2021-02-16 20:43:07 +0000

    Initial commit

---
+++ src/main/java/studente/StudentService.java
@@ -0,0 +1,22 @@
+package studente;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+@Service
+public class StudentService {
+    public List<Student> getStudents(){
+        return List.of(
+                new Student(
+                        1L,
+                        "Marian",
+                        "marian@marian.com",
+                        LocalDate.of(2000, Month.APRIL, 5),
+                        21
+                )
+        );
+    }
+}
